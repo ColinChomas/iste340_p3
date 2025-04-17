3d7d25114462ec63a972353930b2f73ec1c1e22c
colinchomas <colinchomas@msn.com>
2025-04-17 09:48:38 -0400

    Add project files.

---
+++ ChomasP3/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+using System.Threading.Tasks;
+using ChomasP3.Models;
+using ChomasP3.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ChomasP3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            //need to go get the data
+            DataRetrieval dr = new();
+            //tell the instance of dr to go get the data
+            var loadedAbout = await dr.GetData("about/");
+
+            //build our model!
+            //built, called AboutModel !
+
+            //installed NuGet package Newtonsoft.Json
+            var rtnResults = JsonConvert.DeserializeObject<AboutModel>(loadedAbout);
+
+            rtnResults.pageTitle = "About us!";
+
+            return View(rtnResults);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChomasP3/Services/DataRetrieval.cs
@@ -0,0 +1,46 @@
+﻿namespace ChomasP3.Services
+{
+    public class DataRetrieval
+    {
+        // Task vs Thread
+        /* Task has async/await and a return value!
+         * (No direct way to return from a thread)
+         * task can do multiple things at once, thread can do one only
+         * can cancel a task
+         * Task is a higher level concept than a thread
+         */
+
+        //d is something like 'about/' or 'people/'
+        public async Task<string> GetData (string d)
+        {
+            //using statement - at the end of it it automatically calls dispose method
+            using (var client = new HttpClient())
+            {
+                // setup
+                client.BaseAddress = new Uri("https://ischool.gccis.rit.edu/api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                // try/catch
+                try
+                {
+                    HttpResponseMessage res = await client.GetAsync(d, HttpCompletionOption.ResponseHeadersRead);
+                    res.EnsureSuccessStatusCode();
+                    // go get it!
+                    var data = await res.Content.ReadAsStringAsync();
+                    // at this point data is just a string...
+                    return data;
+                } 
+                catch (HttpRequestException hre)
+                {
+                    var msg = hre.Message;
+                    return "HttpReq: " + msg;
+                }
+                catch (Exception ex)
+                {
+                    var msg = ex.Message;
+                    return "Ex: " + msg;
+                }
+            }
+        } 
+    }
+}
